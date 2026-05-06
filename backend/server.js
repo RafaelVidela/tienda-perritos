7df0b3cced0d1ded60a0628c55ba0153b85af887
VA- Alumno <VA-Alumno@duoc.acad>
2026-05-06 14:07:54 -0400

    Prueba DUOC

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+//Rafael Videla Estuvo Aqui
 const {
   DB_HOST = "10.0.159.17", // acá colocar la IP Privada EC2 DB autor: Rafael Videla
   DB_USER = "root",
